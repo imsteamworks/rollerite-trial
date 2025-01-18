29bc72bd462f65627984067d36d35a46ef55b3f6
Steamworks <imsteamworks@gmail.com>
2025-01-18 01:17:10 -0500

    project created

---
+++ src/main/java/dev/fright/rollerite/TrialPlugin.java
@@ -0,0 +1,17 @@
+package dev.fright.rollerite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TrialPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
